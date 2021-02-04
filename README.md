11e19086a1c261032e461bb784009df6b45fc4c2
KarrynS <karrynsaw@gmail.com>
2021-02-05 00:34:29 +1100

    fixing * and deleting notes

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Note-Taker
+## Note-Taker
+
+## Table of Contents
+[Description](#Description) <br>
+[Technologies](#Techonologies) <br>
+[Usage](#Usage) <br>
+[Screenshot](#Screenshot) <br>
+[Contributing](#Contributing) <br>
+[Questions](#questions) <br>
+
+## Description
+An application utilising backend Express.js to allow users to write, store and delete notes. 
+
+## Technologies
+Express.js, Heroku, Javascript, HTML and CSS.
+
+## Usage
+Link to deployed site at Heroku : 
+
+## Screenshot
+<img src="/public/assets/images/Sreenshot.png"/>
+<br>
+<img src="/public/assets/images/SavedNote.png"/>
+
+## Contributing
+To contribute please refer to https://github.com/microsoft/vscode/wiki/How-to-Contribute
+
+## Questions
+Please direct questions to the following email: karrynsaw@gmail.com
+
+
+Github: https://github.com/karryns
+    
---
+++ server.js
@@ -0,0 +1,110 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+const savedNotes = require("./db/db.json");
+
+//Setup Express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Static route that sends user to index.html
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//Inital Routes
+app.get("/", (req,res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+   
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//API Routing - read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res){
+    console.log("hit apinote");
+    console.log(savedNotes)
+    return res.json(savedNotes);
+
+});
+
+//API Routing - Saving and Deleting new notes
+app.post("/api/notes", (req, res) => {
+    const newNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    savedNotes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function(err) {
+        if (err) {
+            return console.log("Error:", err)
+        }
+        console.log("New note successfully saved");
+        res.json(newNote);
+    }); 
+});
+//Deleting notes
+app.delete("/api/notes/:id", (req,res) => {
+    let noteId = req.params.id;
+    let noteIndex = 0;
+    for (let i = 0; i < savedNotes.length; i++) {
+        if (noteId === savedNotes[i].id)
+        noteIndex = i;
+    }
+    savedNotes.splice(noteIndex, 1);
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function(err) {
+        if (err) {
+            return console.log("Error:", err)
+        }
+        console.log("Note successfully deleted");
+        res.json(savedNotes);
+    }); 
+});
+
+
+//Defaulting to index.html
+app.get("*", (req,res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+//Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
+
+
+/* --alternative app.delete
+
+app.delete("/api/notes/:id", (req,res) => {
+    let noteIndex = 0;
+    for(let i = 0; i<savedNotes.length; i++) { 
+        if (req.params.id === savedNotes[i].id) {
+            deleteId = i;
+        }
+    }
+    savedNotes.splice(deleteId, 1);
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function(err) {
+        if(err) {
+            return console.log("Error:", err)
+        }
+        console.log("Note deleted from NoteTaker")
+        res.send(savedNotes);
+    })
+}) 
+app.delete("/api/notes/:id", (req,res) => {
+    const noteId = req.params.id;
+    const deletedNotes = savedNotes.filter(notes => notes.id !== noteId);
+    fs.writeFile("./db/db.json", JSON.stringify(deletedNotes), function(err) {
+        if(err) {
+            return console.log("Error:", err)
+        }
+        console.log("Note deleted from NoteTaker")
+        res.send(deletedNotes);
+    })
+}) 
+*/
